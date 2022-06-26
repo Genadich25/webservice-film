d02c27bc1cc00b5789779fbfd32b7761c8add16c
pin20 <pin200048@gmail.com>
2022-06-26 20:08:54 +0400

    Initial commit

---
+++ src/main/java/com/webservicewithapi/webservicefilm/WebserviceFilmApplication.java
@@ -0,0 +1,13 @@
+package com.webservicewithapi.webservicefilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceFilmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webservicewithapi/webservicefilm/WebserviceFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservicewithapi.webservicefilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
