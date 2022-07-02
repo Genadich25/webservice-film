b2c33acc64c7ba717fafb28dcc11aec0b6189649
pin20 <pin200048@gmail.com>
2022-07-02 19:32:33 +0400

    init branch

--- src/main/java/com/webservicewithapi/webservicefilm/WebserviceFilmApplication.java
+++ src/main/java/com/webservicewithapi/webservicefilm/WebserviceFilmApplication.java
@@ -1,13 +1,16 @@
 package com.webservicewithapi.webservicefilm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebserviceFilmApplication {
-
+    public static final Logger log = LoggerFactory.getLogger(WebserviceFilmApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(WebserviceFilmApplication.class, args);
     }
-
 }
